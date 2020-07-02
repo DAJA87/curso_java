a1172c85e0b0c7316dfe533354d1eb73f5649560
Franklin Mauricio Aguirre Ramirez <franklin.aguirre@Quipux.local>
2020-07-02 08:35:12 -0500

    Cobertura JUNIT

---
+++ PruebaCobertura/src/Prueba/Suscripcion.java
@@ -0,0 +1,33 @@
+package Prueba;
+
+public class Suscripcion {
+	private int precio;
+	private int periodo;
+	
+	public Suscripcion(int precio, int periodo) {
+		this.precio = precio;
+		this.periodo = periodo;
+	}
+	
+	public double precioPorMes() {
+		double resultado;
+		
+		if (periodo <= 0 || precio <= 0) {
+			resultado = 0;
+		}else {		
+			double r = (double) precio / (double) periodo;
+			double resto = r % 1;
+			
+			if (resto > 0) 
+				resultado = r+1;
+			else
+				resultado = r;
+		}
+		
+		return resultado;
+	}
+	
+	public void cancel() {
+		periodo = 0;
+	}
+}
---
+++ PruebaCobertura/src/Prueba/SuscripcionTest.java
@@ -0,0 +1,43 @@
+package Prueba;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SuscripcionTest {
+	@Test
+	public void testPrecioPormes() {
+		double esperado = 12500.0;
+		Suscripcion s = new Suscripcion(25000, 2);
+		double resultado = s.precioPorMes();
+		assertEquals(esperado, resultado, 0);
+	}
+	
+	@Test
+	public void testPrecioPormes2() {
+		double esperado = 8334.333333333334;
+		Suscripcion s = new Suscripcion(25000, 3);
+		double resultado = s.precioPorMes();
+		assertEquals(esperado, resultado, 0);
+	}
+	
+	@Test
+	public void testPrecioPormes3() {
+		double esperado = 0.0;
+		Suscripcion s = new Suscripcion(25000, 0);
+		double resultado = s.precioPorMes();
+		assertEquals(esperado, resultado, 0);
+	}
+	
+	@Test
+	public void testCancel(){
+		Suscripcion s = new Suscripcion(25000, 0);
+		s.cancel();
+	}
+	
+	@Test
+	public void testCancel2(){
+		Suscripcion s = new Suscripcion(0, 0);
+		s.cancel();
+	}
+}
